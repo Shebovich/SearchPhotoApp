7e999b746d7581a765b01d4f29e0827f4e2eff36
Урбанович Павел Валентинович <urbanovichpv@okbtsp.com>
2020-10-13 11:58:47 +0300

    init: created Android Projects and neccesary dirs

---
+++ app/src/main/java/com/shebovich/searchphotoapp/SearchPhotoApplication.kt
@@ -0,0 +1,9 @@
+package com.shebovich.searchphotoapp
+
+import android.app.Application
+
+class SearchPhotoApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
